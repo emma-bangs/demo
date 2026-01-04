9b479184d6e3bb3644a9f366ddc6d50299b82a23
emma-bangs <eb22216007@utg.edu.gm>
2026-01-04 01:19:13 +0000

    Update AppointmentController.java
    I add comments for easy understanding of the code

--- src/main/java/com/hospital/controller/AppointmentController.java
+++ src/main/java/com/hospital/controller/AppointmentController.java
@@ -10,13 +10,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/appointments")
 public class AppointmentController {
-
+// initializing the appointment service
     private final AppointmentService service;
-
+// constructor for initializing attribute
     public AppointmentController(AppointmentService service) {
         this.service = service;
     }
-
+// mapping for the CRUD(Create, Read, Update, & Delete) operations
+    // endpoint for reading all appointments
     @GetMapping
     public List<AppointmentDTO> getAll() {
         return service.getAll().stream()
@@ -30,6 +31,7 @@ public class AppointmentController {
                 .toList();
     }
 
+    // endpoint for reading an appointment by ID
     @GetMapping("/{id}")
     public AppointmentDTO getById(@PathVariable Long id) {
         return service.getById(id)
@@ -44,6 +46,7 @@ public class AppointmentController {
     }
 
 
+    // endpoint for booking an appointment
     @PostMapping
     public AppointmentDTO create(@RequestBody Appointment appointment) {
         Appointment saved = service.save(appointment);
@@ -56,6 +59,7 @@ public class AppointmentController {
         );
     }
 
+    // endpoint for updating an appointment with a given ID
     @PutMapping("/{id}")
     public AppointmentDTO update(@PathVariable Long id, @RequestBody Appointment appointment) {
         appointment.setId(id);
@@ -69,7 +73,7 @@ public class AppointmentController {
         );
     }
 
-
+    // endpoint for deleting an appointment using its ID
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         service.delete(id);
